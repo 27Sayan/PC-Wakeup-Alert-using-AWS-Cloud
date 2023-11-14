712c764a1fde42db5e1525d92114b398383faff5
Sayan <basaksayan2705@gmail.com>
2023-11-14 11:22:48 +0530

    Update awslambda.py

--- awslambda.py
+++ awslambda.py
@@ -2,23 +2,36 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    sesclient = boto3.client("ses", region_name="your_region_name")
+    sesclient = boto3.client("ses", region_name="ap-south-1")
     
-    emailResponse = sesclient.send_email(
-        Destination = {
-            "ToAddresses": [
-                "destination/receiver_email_address"
-            ],
-        },
-        Message={
-            "Body":{
-                "Text":{
-                    "Data": "Email from lambda"
-                }
+    try:
+        emailResponse = sesclient.send_email(
+            Destination = {
+                "ToAddresses": [
+                    "Destination/ Reciever email address"
+                ],
             },
-            "Subject": {
-                "Data": "Email from mes acc"
+            Message={
+                "Body":{
+                    "Text":{
+                        "Data": "Email from lambda"
+                    }
+                },
+                "Subject": {
+                    "Data": "Your PC has been activated."
+                },
             },
-        },
-    Source= "sender_email_address"
-    )
+            Source= "Source/ Sender email address."
+        )
+        response = {
+            "statusCode": 200,
+            "body": json.dumps("Email sent successfully!")
+        }
+    except Exception as e:
+        print(e)
+        response = {
+            "statusCode": 500,
+            "body": json.dumps("Failed to send email.")
+        }
+
+    return response
